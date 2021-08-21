60779cd0793c5b8014eec8917422097328278eb1
arpincheira <arpincheira@uc.cl>
2021-08-21 14:00:18 -0400

    add description information to spacecraft specific page

--- src/pages/specific-spacecraft.page.jsx
+++ src/pages/specific-spacecraft.page.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 import Header from '../components/header.component';
 import { useGetSpacecraftDetailQuery } from '../api/spacecrafts.api';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(() => ({
   imageContainer: {
     height: '100vh',
     width: '100vw',
@@ -17,7 +19,15 @@ const useStyles = makeStyles({
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
   },
-});
+  descriptionTitle: {
+    color: '#F1BD06',
+    opacity: '100%',
+  },
+  descriptionContainer: {
+    backgroundColor: 'rgba(190, 69, 255, 0.2)',
+    textAlign: 'left',
+  },
+}));
 
 export default function SpecificSpacecraftPage() {
   const { id } = useParams();
@@ -26,15 +36,21 @@ export default function SpecificSpacecraftPage() {
 
   return (
     <div className={classes.imageContainer} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${data?.spacecraft_config.image_url})` }}>
-      <Header text={data?.name} textColor="#BE45FF" lineColor="white" backLink="/spacecrafts" />
-      <Grid container>
-        <Grid item>
-          <Typography variant="h3">Description</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h4">{data?.description}</Typography>
-        </Grid>
-      </Grid>
+      <Header text={data?.name} textColor="#BE45FF" lineColor="white" buttonColor="white" backLink="/spacecrafts" />
+      <Container>
+        <Box pt={6}>
+          <Grid container>
+            <Grid className={classes.descriptionContainer} container item direction="column" justifyContent="space-between" wrap="nowrap" alignItems="flex-start" xs={12} md={6}>
+              <Grid item>
+                <Typography className={classes.descriptionTitle} variant="h5">Description</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">{data?.description}</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
 }
