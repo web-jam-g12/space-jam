e03b316fac40b0b8ddb55065590f03c10c8a7b64
humbertoortuzar <hjortuzar@uc.cl>
2021-08-21 12:26:09 -0400

    not found routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,12 +5,14 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/home.page';
+import NotFound from './pages/404notfound';
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
