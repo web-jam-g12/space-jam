d1a64acc8701068ee72ce251ae3d5e99304434fa
meretamal <meretamal@uc.cl>
2021-08-21 15:05:07 -0400

    add a pagination component

---
+++ src/components/pagination.component.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  field: {
+    border: '1px solid white',
+  },
+  whiteColor: {
+    color: 'white',
+  },
+}));
+
+export default function Pagination({ min, max, onChange }) {
+  const [page, setPage] = useState(parseInt(min, 10));
+  const classes = useStyles();
+
+  useEffect(() => {
+    onChange(page);
+  }, [page]);
+
+  return (
+    <Grid container justifyContent="space-around">
+      <Grid item xs="2">
+        <IconButton onClick={() => setPage(page - 1)} disabled={min === page}>
+          <ChevronLeftIcon style={{ color: 'white' }} />
+        </IconButton>
+      </Grid>
+      <Grid item xs="2">
+        <TextField className={classes.field} inputProps={{ className: classes.whiteColor }} color="primary" type="number" variant="outlined" size="small" value={page} onChange={(e) => setPage(e.target.value)} />
+      </Grid>
+      <Grid item xs="4">
+        <Typography>
+          of
+          {' '}
+          {max}
+        </Typography>
+      </Grid>
+      <Grid item xs="2">
+        <IconButton onClick={() => setPage(parseInt(page, 10) + 1)}>
+          <ChevronRightIcon style={{ color: 'white' }} />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+}
+
+Pagination.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Pagination.defaultProps = {
+  min: 1,
+};
