94b14ee2c23768cc949b2ad1eaa07c4598f98b26
humbertoortuzar <hjortuzar@uc.cl>
2021-08-21 15:56:50 -0400

    change in styles and info

--- src/pages/comingSoon.jsx
+++ src/pages/comingSoon.jsx
@@ -5,18 +5,16 @@ import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
-//  import spaceImage from '../assets/images/nasa-Yj1M5riCKk4-unsplash.jpg';
-
 const useStyles = makeStyles({
   mainPageImage: {
-    height: '100vh',
+    minHeight: '100vh',
     width: '100vw',
     position: 'relative',
     backgroundSize: 'cover',
     textAlign: 'center',
-    backgroundPosition: 'center',
+    backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
-    backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://i.guim.co.uk/img/media/523656697e208a8de3169794a5020b3a242ef412/0_145_3032_1819/master/3032.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f2c74c82895f1de1b5c901f411938161')",
+    backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://images.unsplash.com/photo-1614315517650-3771cf72d18a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80)',
   },
   overlay: {
     justifyContent: 'center',
@@ -30,25 +28,35 @@ const useStyles = makeStyles({
     width: '100vw',
     height: '75vh',
   },
+  quote: {
+    maxWidth: '75vw',
+    textAlign: 'center',
+  },
+  titleText: {
+    color: '#FFFFFF',
+    textAlign: 'left',
+    padding: '2vw',
+  },
 });
 
 export default function ComingSoon() {
   const classes = useStyles();
   return (
     <div className={classes.mainPageImage}>
+      <Typography variant="h2" className={classes.titleText}>Coming Soon...</Typography>
+      <Typography variant="h5" className={classes.titleText}>Going Far and Beyond!</Typography>
       <div className={classes.overlay}>
         <Grid container direction="column" justifyContent="space-evenly" wrap="nowrap" className={classes.GridContainer}>
           <Grid item>
-            <Typography variant="h2" style={{ color: '#FFFFFF' }}>Coming Soon...</Typography>
+            <Typography variant="body1">“The Earth is the cradle of humanity, but mankind cannot stay in the cradle forever.”</Typography>
+            <Typography variant="body2">Konstantin Tsiolkovsky</Typography>
           </Grid>
           <Grid item>
-            <Typography variant="body1">“The eternal silence of these infinite spaces frightens me.”</Typography>
-            <Typography variant="body2">Blais Pascal</Typography>
-          </Grid>
-          <Grid item>
-            <Link component={RouterLink} underline="none" to="/">
-              <Typography variant="body2">Go to Homepage</Typography>
-            </Link>
+            <Typography variant="body2">
+              <Link component={RouterLink} color="inherit" underline="none" to="/">
+                Go to Homepage
+              </Link>
+            </Typography>
           </Grid>
         </Grid>
       </div>
