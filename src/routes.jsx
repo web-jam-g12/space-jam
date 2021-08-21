34d0c7894c6c889cae09f67588fd5f9f9a922b9a
arpincheira <37157550+arpincheira@users.noreply.github.com>
2021-08-21 14:52:50 -0400

    Feat/specific spacecraft page (#10)
    * add initial specific spacecraft page
    
    * add description information to spacecraft specific page
    
    * add final desing

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,7 @@ import AstronautListPage from './pages/astronaut-list.page';
 import HomePage from './pages/home.page';
 import NotFound from './pages/404notfound';
 import SpacecraftListPage from './pages/scapecraft-list.page';
+import SpecificSpacecraftPage from './pages/specific-spacecraft.page';
 
 export default function Routes() {
   return (
@@ -16,6 +17,7 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/spacecrafts" component={SpacecraftListPage} />
+        <Route exact path="/spacecrafts/:id" component={SpecificSpacecraftPage} />
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/astronauts" component={AstronautListPage} />
         <Route component={NotFound} />
