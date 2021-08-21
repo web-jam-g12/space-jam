664768c9d1bdac81ad66827853f00442ba7be072
arpincheira <arpincheira@uc.cl>
2021-08-21 14:38:33 -0400

    add final desing

--- src/pages/specific-spacecraft.page.jsx
+++ src/pages/specific-spacecraft.page.jsx
@@ -9,9 +9,9 @@ import Grid from '@material-ui/core/Grid';
 import Header from '../components/header.component';
 import { useGetSpacecraftDetailQuery } from '../api/spacecrafts.api';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   imageContainer: {
-    height: '100vh',
+    minHeight: '100vh',
     width: '100vw',
     position: 'relative',
     backgroundSize: 'cover',
@@ -19,13 +19,18 @@ const useStyles = makeStyles(() => ({
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
   },
-  descriptionTitle: {
+  informationTitle: {
     color: '#F1BD06',
-    opacity: '100%',
+    paddingLeft: theme.spacing(3),
+    paddingTop: theme.spacing(2),
   },
-  descriptionContainer: {
-    backgroundColor: 'rgba(190, 69, 255, 0.2)',
+  informationContent: {
     textAlign: 'left',
+    padding: theme.spacing(2),
+  },
+  informationContainer: {
+    backgroundColor: 'rgba(190, 69, 255, 0.3)',
+    border: '1px solid #BE45FF',
   },
 }));
 
@@ -33,19 +38,30 @@ export default function SpecificSpacecraftPage() {
   const { id } = useParams();
   const { data } = useGetSpacecraftDetailQuery(id);
   const classes = useStyles();
-
   return (
     <div className={classes.imageContainer} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${data?.spacecraft_config.image_url})` }}>
       <Header text={data?.name} textColor="#BE45FF" lineColor="white" buttonColor="white" backLink="/spacecrafts" />
       <Container>
         <Box pt={6}>
           <Grid container>
-            <Grid className={classes.descriptionContainer} container item direction="column" justifyContent="space-between" wrap="nowrap" alignItems="flex-start" xs={12} md={6}>
+            <Grid className={classes.informationContainer} container item direction="column" justifyContent="space-between" wrap="nowrap" alignItems="flex-start" xs={12} md={6}>
               <Grid item>
-                <Typography className={classes.descriptionTitle} variant="h5">Description</Typography>
+                <Typography className={classes.informationTitle} variant="h5">Description</Typography>
               </Grid>
               <Grid item>
-                <Typography variant="body1">{data?.description}</Typography>
+                <Typography className={classes.informationContent} variant="body1">{data?.description}</Typography>
+              </Grid>
+              <Grid item container>
+                <Typography className={classes.informationTitle} variant="h5">Serial Number </Typography>
+                <Typography className={classes.informationContent} variant="body1">{data?.serial_number}</Typography>
+              </Grid>
+              <Grid item container>
+                <Typography className={classes.informationTitle} variant="h5">Status </Typography>
+                <Typography className={classes.informationContent} variant="body1">{data?.status.name}</Typography>
+              </Grid>
+              <Grid item container>
+                <Typography className={classes.informationTitle} variant="h5">Agency Name </Typography>
+                <Typography className={classes.informationContent} variant="body1">{data?.spacecraft_config.agency.name}</Typography>
               </Grid>
             </Grid>
           </Grid>
