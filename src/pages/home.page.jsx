63475629bc349ef36b14cdfe989fb0618a89fa7b
arpincheira <arpincheira@uc.cl>
2021-08-21 08:27:06 -0400

    add initial main page

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+import spaceImage from '../assets/images/nasa-Yj1M5riCKk4-unsplash.jpg';
+
+const useStyles = makeStyles({
+  mainPageImage: {
+    height: '100vh',
+    width: '100vw',
+    position: 'absolute',
+    backgroundSize: 'cover',
+    textAlign: 'center',
+    backgroundImage: `url(${spaceImage})`,
+  },
+  overlay: {
+    color: '#FFFFFF',
+    justifyContent: 'center',
+    fontFamily: 'Orbitron',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '60px',
+    lineHeight: '60px',
+  },
+});
+
 export default function HomePage() {
-  return <Typography variant="h1">Home Page</Typography>;
+  const classes = useStyles();
+  return (
+    <div className={classes.mainPageImage}>
+      <div className={classes.overlay}>
+        <Typography variant="h2">Welcome to your Journey through Space</Typography>
+      </div>
+    </div>
+  );
 }
